2ac757bc5444100e0dbd9a7eda9684916e6e0f4d
Dante Torres <isc_s80@hotmail.com>
2022-03-29 22:15:25 -0600

    Tercer Commit

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -5,11 +5,12 @@
          type="text"
          class="form-control"
          placeholder="Buscar Entrada"
+         v-model = 'term'
         >
       </div>
 
       <div class="entry-scrollarea">
-       <EntryComp v-for="item in 100 " :key="item">
+       <EntryComp v-for="item in entriesByTerm" :key="item">
             Entry
        </EntryComp>
                     
@@ -19,12 +20,28 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
+import { mapGetters } from 'vuex'
 
 export default {
 
     components: {
         EntryComp: defineAsyncComponent( ()=> import('./EntryComp.vue') )
+    },
+    computed:{
+        ...mapGetters('journal', ['getEntriesByTerm']),
+        entriesByTerm(){
+
+            return this.getEntriesByTerm( this.term )
+        }
+    },
+    data(){
+
+     return {
+         term: ''
+     }
+
     }
+    
 
 }
 </script>
--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,8 +1,11 @@
 
 
 
-export const  getEntriesByTerm=  ( /* state */ ) =>{
+export const  getEntriesByTerm =  ( state  ) => ( term = '') =>{
     
+    if ( term.length === 0 ) return state.entries
+    
+    return state.entries.filter( entry => entry.text.toLowerCase().includes( term.toLowerCase() ) )
 
 }
 
