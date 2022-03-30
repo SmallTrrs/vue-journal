40a670bfd605369056c972aa269d3c5256a40120
Dante Torres <isc_s80@hotmail.com>
2022-03-29 21:49:01 -0600

    Segundo Commit

---
+++ src/modules/daybook/store/journal/actions.js
@@ -0,0 +1,18 @@
+
+
+export const loadEntries = async (/*{ commit }*/) =>{
+
+     
+
+}
+
+export const createEntries = async (/*{ commit }*/) =>{
+
+     
+
+}
+export const updateEntries = async (/*{ commit }*/) =>{
+
+     
+
+}
---
+++ src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,15 @@
+
+
+export const setEntries =  ( /* state */ ) =>{
+
+
+}
+
+export const createEntry =  ( /* state */ ) =>{
+
+
+}
+export const updateEntry =  ( /* state */ ) =>{
+
+
+}
